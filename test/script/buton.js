7ad081e9fe72cc7c47417e8f7cf5f2e18c53bda5
kikinho11 <fca.10.fa@gmail.com>
2023-09-01 14:50:18 +0100

    JS script working

--- test/script/buton.js
+++ test/script/buton.js
@@ -1,17 +1,16 @@
-const elementoButao = document.querySelector('saber-mais');
-
-const elementoTexto = document.querySelector('texto-escondido');
+const elementoButao = document.getElementById("saber-mais");
+const elementoTexto = document.getElementById('texto-escondido');
+console.log(elementoTexto);
 
 elementoButao.addEventListener('click', function() {
+  if (elementoTexto.style.display === "none") {
 
-  if(elementoTexto.style.display == "none") {
-
-    elementoTexto.style.display == "block";
+    elementoTexto.style.display = "block";
 
   } else {
 
-    elementoTexto.style.display == "none";
+    elementoTexto.style.display = "none";
 
   }
 
-})
+});
