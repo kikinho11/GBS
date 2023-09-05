3e315cfec37658aa0867c762b60be8eb907619bb
kikinho11 <fca.10.fa@gmail.com>
2023-09-05 14:08:03 +0100

    Updated for CSS adjustments

--- test/script/mainPage.js
+++ test/script/mainPage.js
@@ -5,23 +5,23 @@ navLinks.forEach(el => {
   el.addEventListener('click', function(event) {
     if(!el.classList.contains('active')) {
       event.preventDefault();
-
+      
       if (previous != null && previous !== el) {
         previous.style.color = '#fff';
         previous.classList.remove('active');
       }
-
+      
       el.style.color = '#854DFF'; // Alterar a cor do clique
       el.classList.add('active');
-
+      
       previous = el;
-
+      
       const targetId = el.getAttribute('href');
       console.log(targetId);
       const targetSection = document.querySelector(targetId);
       console.log(targetSection);
       const offset = targetSection.offsetTop - 70; // 120 is the height of the navbar
-
+      
       if (targetSection) {
         window.scrollTo({
           top: offset,
@@ -31,3 +31,14 @@ navLinks.forEach(el => {
     }
   })
 })
+
+let gotoTop = document.querySelector('.topo-site');
+
+gotoTop.addEventListener('click', function(){
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+
